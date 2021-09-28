23e024b59af8a96636347bf01e82af765e1f80cb
TamaraLavrina <tamara.lavrina@gmail.com>
2021-09-28 14:24:24 +0300

    feature: добавляет вспомогательные функции

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 const getRandomInteger = (min, max) => {
-  if (min < 0 && max < min) {
-    throw new SyntaxError("Неверный интервал, минимальное значение должно быть больше 0 и меньше максимального значения");
+  if (min <= 0 && max < min) {
+    throw new SyntaxError("Неверный интервал, минимальное значение должно быть больше или равно 0 и меньше максимального значения");
   }
   else {
     const rand = min + Math.random() * (max + 1 - min);
