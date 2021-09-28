7883d551b5562a72470dab7975f1429088be6548
TamaraLavrina <tamara.lavrina@gmail.com>
2021-09-28 19:37:59 +0300

    :fix: правит ошибки

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,14 @@
 const getRandomInteger = (min, max) => {
   if (min <= 0 && max < min) {
-    throw new SyntaxError("Неверный интервал, минимальное значение должно быть больше или равно 0 и меньше максимального значения");
+    throw new SyntaxError('Неверный интервал, минимальное значение должно быть больше или равно 0 и меньше максимального значения');
   }
   else {
     const rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
-}
+};
 // источник - learn.js
 
-const checkMaxLength = (comment, maxLength) => {
-  if (comment.toString().length <= maxLength) { return true }
-  else { return false }
-};
-
+const checkMaxLength = (comment, maxLength = 140) => comment.toString().length <= maxLength;
 
 export {getRandomInteger, checkMaxLength};
